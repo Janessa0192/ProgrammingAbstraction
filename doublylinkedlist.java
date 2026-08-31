d2dda4f63b11e04d18275ca850a9bd43609e574c
Janessa <janessa3035.beai24@chitkara.edu.in>
2026-08-31 11:20:37 +0530

    C.Work

---
+++ doublylinkedlist.java
@@ -0,0 +1,117 @@
+package com.masai;
+
+public class DoublyLinkedList {
+
+	
+	static class Node{
+		
+		int data;
+		Node prev;
+		Node next;
+		
+		Node(int data){
+			this.data = data;
+			this.prev = null;
+			this.next = null;
+		}
+		
+		
+	}
+
+	
+	
+	
+		public static void main(String[] args) {
+			
+			
+			
+		    Node head = new Node(10);
+			Node second = new Node(20);
+            Node third = new Node(30);
+            Node fourth = new Node(40);
+            
+            
+            head.next = second;
+            second.prev=head;
+            
+            second.next=third;
+            
+            third.prev=second;
+            third.next = fourth;
+            fourth.prev = third;
+            
+            
+//            Create a doubly linked list and take two values from the user: 
+//            key and value. Insert the new node after the node containing key.
+            
+//            int key = 30; int val = 40;
+//            
+//            Node temp = head;
+//            while(temp!=null) {
+//            	
+//            	
+//            	
+//            	if(temp.data == key) {
+//            		
+//            		Node value = new Node(val);
+//            		if(temp.next!=null) {
+//            			temp.next.prev = value;
+//            		}
+//            		value.next = temp.next;
+//            		temp.next = value;
+//            		value.prev = temp;
+//            	}
+//            	temp = temp.next;
+//              }
+//            
+
+//            remove all occurences of value
+//            int value = 20;
+//            
+//            Node temp = head;
+//            
+//            // 20 - 20 - 30 - 40
+//            
+//            while(temp!=null) {
+//            	
+//            	if(temp.data == value) {
+//            		if(temp.prev!=null) {
+//                		temp.prev.next = temp.next;
+//                	} else {
+//                		head = temp.next;
+//                	}
+//                	
+//                	if(temp.next!=null) {
+//                		temp.next.prev = temp.prev;
+//                	}
+//            	}
+//            	temp = temp.next;
+//            }
+            
+//            reverse doubly linked list
+//            Node temp = head;Node newhead = null;
+//            
+//            while(temp!=null) {
+//            	
+//            	Node swap = temp.prev;
+//            	temp.prev = temp.next;
+//            	temp.next = swap;
+//            	
+//            	newhead = temp;
+//             
+//             temp = temp.prev;
+//            }
+//            
+//            head = newhead;
+            
+            
+            
+            
+           // print DLL
+          Node curr = head;
+          while(curr!=null) {
+          	System.out.println(curr.data);
+          	curr = curr.next;
+          }
+            
+		}}
