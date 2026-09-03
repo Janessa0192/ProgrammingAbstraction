7bc5b882778a7303d50227e5dd184ea5fbe2a6d9
Janessa <janessa3035.beai24@chitkara.edu.in>
2026-09-03 11:31:40 +0530

    swap nodes in pairs

---
+++ LinkedLists/leetcode24.java
@@ -0,0 +1,20 @@
+class Solution {
+    public ListNode swapPairs(ListNode head) {
+        
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+        ListNode prev = dummy;
+       
+
+        while(prev.next!=null && prev.next.next!=null){
+        ListNode first = prev.next;
+        ListNode second = first.next;
+        first.next = second.next;
+        prev.next = second;
+        second.next = first;
+        prev = second.next;
+        }
+
+        return dummy.next;
+    }
+}
