235a8fc12da0205bd37f1f90d9a49ccdbb2c9935
Janessa <janessa3035.beai24@chitkara.edu.in>
2026-09-20 19:33:17 +0530

    next greater element 1

---
+++ Stacks/leetcode496.java
@@ -0,0 +1,32 @@
+class Solution {
+    public int[] nextGreaterElement(int[] nums1, int[] nums2) {
+        
+        int n = nums2.length;
+
+        Stack<Integer> st = new Stack<>();
+        HashMap<Integer,Integer> map = new HashMap<>();
+        
+        for(int i=n-1;i>=0;i--){
+              while(!st.isEmpty() && nums2[i]>=st.peek()){
+                st.pop();
+              }
+
+              map.put(nums2[i], st.isEmpty() ? -1 : st.peek());
+
+              
+
+              st.push(nums2[i]);
+
+        }
+
+        int[] ans = new int[nums1.length];
+
+        for (int i = 0; i < nums1.length; i++) {
+            ans[i] = map.get(nums1[i]);
+        }
+
+        return ans;
+
+      
+    }
+}
